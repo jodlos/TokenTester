1b9b561244a7e91590b9716dcff74f9fcd7647f4
jodlos <jodlos@student.wszib.edu.pl>
2023-01-07 09:21:37 +0100

    Nowe

---
+++ src/SquareRootAplication.java
@@ -0,0 +1,10 @@
+public class SquareRootAplication {
+    public static void main(String[] args) {
+
+        double a = Double.parseDouble(args[0]);
+        System.out.println(Math.sqrt(a));
+
+        }
+
+
+    }
